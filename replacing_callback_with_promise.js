04d87ea21e457e2ed5aa9036d617ef92337ccf9c
ThisIsSandeep <thesandybansal@gmail.com>
2018-09-22 12:35:56 +0530

    Async and Await approach

--- replacing_callback_with_promise.js
+++ replacing_callback_with_promise.js
@@ -1,9 +1,24 @@
 console.log('Before');
+// Promise based approach
 getUser(1)
   .then(user => getRepo(user.userName))
   .then(repo => getCommits(repo[0]))
   .then(commits => console.log(commits))
   .catch(err => console.log('Error: ', err.message));
+
+// Async and await approach
+async function displayCommit() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepo(user.userName);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log('Error', err.message);
+  }
+}
+
+displayCommit();
 
 console.log('After');
 
@@ -21,6 +36,7 @@ function getRepo(userName) {
     setTimeout(() => {
       console.log('Getting user repo..... of', userName);
       resolve(['repo1', 'repo2', 'repo3', 'repo4']);
+      // reject(new Error('Could not get the repo'));
     }, 2000);
   });
 }
